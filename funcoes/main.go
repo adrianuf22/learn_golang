5062a321a71c64f24cc65ce94ada54b8674004d5
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-26 16:52:08 -0200

    Rename funcoes_1 folder

---
+++ funcoes/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adrianuf22/learn_golang/funcoes_1/matematica"
+)
+
+func main() {
+	resultado := matematica.Calc(matematica.Multiplicar, 2, 3)
+
+	fmt.Printf("O resultado de 2 * 3 é igual: %d\r\n", resultado)
+
+	// @v2
+	// In this case, uses = instead of :=, cause now we are just defining a value
+	// to an created variable
+	resultado = matematica.Calc(matematica.Soma, 3, 4)
+	fmt.Printf("O resultado de 3 + 4 é igual: %d\r\n", resultado)
+
+	// @v7
+	// resultado = matematica.Calc(matematica.Dividir, 2, 2)
+	// fmt.Printf("O resultado de 2 / 2 é igual: %d\r\n", resultado)
+	// @v8
+	// Now, uses := cause resto variable was not defined yet
+	resultado, resto := matematica.Dividir(2, 2)
+	fmt.Printf("O resultado de 2 / 2 é igual: %d\r\n", resultado)
+	fmt.Printf("E o resto de 2 / 2 é: %d\r\n", resto)
+}
+
+// @v5 func multiplicar(x int, y int) int {
---
+++ funcoes/matematica/soma.go
@@ -0,0 +1,9 @@
+package matematica
+
+// Capitalized: Exports the value
+/**
+	Soma dois valores inteiros
+*/
+func Soma(a int, b int) int {
+	return a + b
+}
