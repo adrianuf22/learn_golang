c32ec2402e607f22c816c5bceee62a5927ff7712
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-26 14:54:02 -0200

    Fix not used packages

--- pacotes/main.go
+++ pacotes/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 
-	// Wrong - Importing a local package from GOPATH
-	"pacotes/prefixo"
-	// Right - Importing a local package
+	"github.com/adrianuf22/learn_golang/pacotes/prefixo"
 	"github.com/adrianuf22/learn_golang/pacotes/operadora"
 )
 
@@ -15,4 +13,5 @@ var NomeDoUsuario = "Adriano"
 func main() {
 	fmt.Printf("Nome do usuario: %s\r\n", NomeDoUsuario)
 	fmt.Printf("Prefixo da Capital: %d\r\n", prefixo.Capital)
+	fmt.Printf("Operadora: %s\r\n", operadora.NomeOperadora)
 }
