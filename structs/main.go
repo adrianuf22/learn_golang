6919d2a1e4cb648416100427cb562cad9fe33f84
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-26 17:29:52 -0200

    Lea structs advanced

--- structs/main.go
+++ structs/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-// Defines a new type Imovel
-type Imovel struct {
+//Imovel é um type que armazena dados de um imovel
+type Imovel struct { // Defines a new type Imovel
 	Size int // Capitalized properties are exported
 	Garage bool
 	Name string // A default value also is defined when nothing is passed
@@ -15,8 +15,34 @@ type Imovel struct {
 func main() {
 	// Creates a variable with the new type
 	casa := Imovel{}
-
 	fmt.Printf("%v\r\n", casa)
 
-	
+	// Creates a variable passing the values to type initialization
+	fazenda := Imovel{2000, true, "Uataragaa", 3500}
+	fmt.Printf("%v\r\n", fazenda)
+
+	// Defining a single or partial value to type initialization
+	// Wrong
+	// apartamento := Imovel{55}
+	// fmt.Printf("%v\r\n", apartamento)
+
+	// Right
+	// Uses an spaces between type name and braces
+	apartamento := Imovel {
+		value: 55, // Trailling commas are required and nice!
+	}
+	fmt.Printf("%v\r\n", apartamento)
+
+	// Passing values in different order
+	chacara := Imovel {
+		Garage: true,
+		value: 400,
+		Name: "Mirapora",
+		Size: 680,
+	}
+	fmt.Printf("%v\r\n", chacara)
+
+	// Setting a property value
+	casa.Name = "Adams family's house"
+	fmt.Printf("%v\r\n", casa)
 }
