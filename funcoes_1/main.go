a7c8590d70a6a530528ef9a867d7ab926bf775cf
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-26 16:41:24 -0200

    Lea funcoes

--- funcoes_1/main.go
+++ funcoes_1/main.go
@@ -7,15 +7,24 @@ import (
 )
 
 func main() {
-	resultadoDaConta := matematica.Calc(matematica.Multiplicar, 2, 3)
+	resultado := matematica.Calc(matematica.Multiplicar, 2, 3)
 
-	fmt.Printf("O resultado de 2 * 3 é igual: %d\r\n", resultadoDaConta)
+	fmt.Printf("O resultado de 2 * 3 é igual: %d\r\n", resultado)
 
 	// @v2
 	// In this case, uses = instead of :=, cause now we are just defining a value
 	// to an created variable
-	resultadoDaConta = matematica.Calc(matematica.Soma, 3, 4)
-	fmt.Printf("O resultado de 3 + 4 é igual: %d\r\n", resultadoDaConta)
+	resultado = matematica.Calc(matematica.Soma, 3, 4)
+	fmt.Printf("O resultado de 3 + 4 é igual: %d\r\n", resultado)
+
+	// @v7
+	// resultado = matematica.Calc(matematica.Dividir, 2, 2)
+	// fmt.Printf("O resultado de 2 / 2 é igual: %d\r\n", resultado)
+	// @v8
+	// Now, uses := cause resto variable was not defined yet
+	resultado, resto := matematica.Dividir(2, 2)
+	fmt.Printf("O resultado de 2 / 2 é igual: %d\r\n", resultado)
+	fmt.Printf("E o resto de 2 / 2 é: %d\r\n", resto)
 }
 
 // @v5 func multiplicar(x int, y int) int {
--- funcoes_1/matematica/generico.go
+++ funcoes_1/matematica/generico.go
@@ -9,3 +9,18 @@ func Calc(fn func(int, int) int, a int, b int) int {
 func Multiplicar(x int, y int) int {
 	return x * y;
 }
+
+// @v7
+//Dividir divide o valor a pelo b
+// func Dividir(a int, b int) (resultado int) { // Using a named return value
+// 	resultado = a / b // Named return value are automatic defined by Go, so just uses = to set the value
+// 	return // Cause I using a named return value, an empty return must be used here
+// }
+// @v8
+// Dividir divide o valor a pelo b
+// Retorna o valor da divisão e o resto
+func Dividir(a int, b int) (resultado int, resto int) { // To returns multiples values
+	resultado = a / b
+	resto = a % b
+	return // Same as returns a single value
+}
