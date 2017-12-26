e66b3ff26f1a3805332ec5e6b90fac412de45ef7
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-26 15:52:35 -0200

    Lea import cycle

--- pacotes/prefixo/numeros.go
+++ pacotes/prefixo/numeros.go
@@ -1,4 +1,16 @@
 package prefixo
 
+import (
+	// @v5 Cycle import
+	// "github.com/adrianuf22/learn_golang/pacotes/operadora"
+)
+
 //Capital prefixo da capital de Sao Paulo
 var Capital int = 11
+
+// NomeOperadoraPrefixoInterior nome da operadora e prefixo do interior
+// @v5 var NomeOperadoraPrefixoInterior = operadora.NomeOperadora + " - 13"
+// The code above does not compiles. => import cycle not allowed
+// The current package already been imported in operadora package and now,
+// the package was imported inside a package that already has them
+// This is not allowed by Go compiler
