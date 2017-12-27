957162425fd60e933318aa72376606932910a86a
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-27 17:42:23 -0200

    Lea basic of slices

---
+++ slices/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// An Slice
+	var listaDeNumeros []int
+	// Unlike Array, an Slice hasn't size defined and values can be setted at any moment
+	// Slices are like Arrays to another languages, as PHP and Javascript
+	// Every a new value was setted, all Array is copied to a new Array with a new size 
+
+	// Wrong - Setting a value in a pure Slice
+	// listaDeNumeros[0] = 1 // index out of range
+	// Right - Setting a value (Or, Appending) in a pure Slice
+	listaDeNumeros = append(listaDeNumeros, 1)
+
+	fmt.Println("O que há na lista de números?", listaDeNumeros)
+
+	// Creates an Slice and initialize 3 spaces
+	listaDeNumeros = make([]int, 3)
+	fmt.Println("O que há na lista de números?", listaDeNumeros)
+
+	// Setting a value in a initialized Slice
+	listaDeNumeros[0] = 1
+	listaDeNumeros[1] = 2
+	listaDeNumeros[2] = 3
+	fmt.Println("O que há na lista de números?", listaDeNumeros)
+
+	// Getting the length and capacity of a Slice
+	fmt.Println(listaDeNumeros, len(listaDeNumeros), cap(listaDeNumeros))
+}
