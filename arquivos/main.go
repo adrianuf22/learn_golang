e564408a5b2ec17dc66f42cc5b3f2f63148ede00
adrianuf22 <adrianu_f22@hotmail.com>
2017-12-28 12:15:57 -0200

    Lea reading csv files

--- arquivos/main.go
+++ arquivos/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"encoding/csv"
 )
 
 func main() {
@@ -18,5 +19,43 @@ func main() {
 	for scanner.Scan() { // Iterate file lines
 		fmt.Println("O conteudo da linha é: ", scanner.Text()) // get text from line
 	}
- 	nomes.Close() // Close the opened file
+	// @v2 To the end
+	// @v3 This file must die!
+	nomes.Close() // Close the opened file
+	
+	// Reading the same file twice
+	// Files are streams and once readed everything will go away
+	// Trys to read same file now returns nothing
+	// Solutions by the moment: Read the file again
+	
+	// Reading again
+	nomesReadedAgain, _ := os.Open("nomes.csv")
+
+	// Wrong - Working with CSV
+	nomesCSV := csv.NewReader(nomesReadedAgain)
+	conteudo, err := nomesCSV.ReadAll()
+	if err != nil {
+		fmt.Println("Houve um erro ao ler o conteudo do arquivo CSV. Erro: ", err.Error())
+		return
+	}
+
+	if 0 == len(conteudo) {
+		fmt.Println("O conteúdo está vazio!")
+		return
+	}
+
+	// Reading line by line
+	for indice, linha := range conteudo {
+		fmt.Printf("Na linha #%d o conteudo é %s\r\n", indice, linha)
+		
+		// Reading item by item
+		for indiceItem, item := range linha {
+			fmt.Printf("No item #%d o valor é %s\r\n", indiceItem, item)
+		}
+	}
+
+	// @v2
+	// nomes.Close()
+	// @v3
+	nomesReadedAgain.Close()
 }
